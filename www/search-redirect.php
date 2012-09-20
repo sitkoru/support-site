0aefda0bee6e1547b4abb86c09dee28e15597e65
Dekmabot <dekmabot@gmail.com>
2012-09-20 14:33:18 +0600

    search

--- www/search-redirect.php
+++ www/search-redirect.php
@@ -1,6 +1,9 @@
 <?php
 
-print_r( $_GET );
+$f = fopen('search.log', 'a+');
+fwrite($f, date("Y-m-d H:i:s").': ' .serialize( $_GET ) . "\n");
+fclose( $f );
+
 exit();
 
 header('Location: /search.html?q='.$_GET['q']);
